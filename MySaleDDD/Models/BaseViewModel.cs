21b1efb239cf6bdeb8097c2a2abb16695a1418ca
a.rezaei0 <a.rezaei0@yahoo.com>
2020-12-30 13:43:22 +0330

    99/10/10-13:43

--- MySaleDDD/Models/BaseViewModel.cs
+++ MySaleDDD/Models/BaseViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MySaleDDD.Resources;
+
+
 
 namespace MySaleDDD.Models
 {
@@ -10,6 +14,7 @@ namespace MySaleDDD.Models
         public int Id { get; set; }
         public DateTime? AddedDate { get; set; }
         public string SystemUserId { get; set; }
+      
         public string Titel { get; set; }
     }
 }
